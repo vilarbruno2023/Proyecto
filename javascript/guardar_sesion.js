b199b50b3cb85bee038b60e69f571a077cc3a45d
urito2 <federico27gonz@gmail.com>
2026-08-31 19:39:13 -0300

    Delete javascript directory

--- javascript/guardar_sesion.js
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-
-if (params.get('login') === 'ok') {
-    localStorage.setItem('usuario', params.get('u'));
-    localStorage.setItem('rol', params.get('r'));
-
-    window.history.replaceState({}, document.title, window.location.pathname);
-}
