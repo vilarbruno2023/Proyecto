0ca060b800f60be0fe9638131d5b3409ce8dec75
urito2 <federico27gonz@gmail.com>
2026-08-31 19:38:46 -0300

    Delete api directory

--- api/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$_SESSION = [];
-
-session_destroy();
-
-header("Content-Type: application/json");
-echo json_encode(['logout' => true, 'redirect' => '../htmls/inicio.html']);
